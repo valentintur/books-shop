d8ef6c6ef9511b37ff42a8e644cb439bb817fd55
Valentin Tur <valentin.tur@pageart.pl>
2022-05-24 01:18:39 +0200

    feat: add all  validations

--- assets/scripts/check-form.js
+++ assets/scripts/check-form.js
@@ -23,4 +23,53 @@ function formatDate(date) {
     return [year, month, day].join('-');
 }
 
+const checkBoxLimit = () => {
+	let checkBoxGroup = document.forms['order-form']['check-gift'];			
+	let limit = 2;
+	for (let i = 0; i < checkBoxGroup.length; i++) {
+		checkBoxGroup[i].onclick = function() {
+			let checkedcount = 0;
+			for (let i = 0; i < checkBoxGroup.length; i++) {
+				checkedcount += (checkBoxGroup[i].checked) ? 1 : 0;
+			}
+			if (checkedcount > limit) {
+				console.log("You can select maximum of " + limit + " checkboxes.");
+				alert("You can select maximum of " + limit + " checkboxes.");						
+				this.checked = false;
+			}
+		}
+	}
+}
+
+const checkForm = () => {
+    let elements = document.forms['order-form'].elements;
+    let checkingArr = Array.from(elements);
+    
+    let cansubmit = true;
+    for(let i = 0; i < checkingArr.length; i++) {
+        if((checkingArr[i].value.length == 0 || !checkingArr.some(el => el.checked)) && checkingArr[i].type != "button")
+        {
+            cansubmit = false;
+        }
+
+    }
+
+    //console.log(elements);
+    // Array.from(inputs).some(input => input.checked)
+
+    // const payCard = document.getElementById('payment-type-card');
+    // const payCash = document.getElementById('payment-type-cash');
+
+    // console.log(payCard);
+    // console.log(payCash);
+    // console.log(payCard.checked);
+    // console.log(payCash.checked);
+    document.getElementById("submit-button").disabled = !cansubmit;  
+};
+
+
 checkDate();
+checkBoxLimit();
+//checkForm();
+setInterval(checkForm, 1000);
+//();
